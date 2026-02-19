048ff1d0241389476e6e15255619041ee7ec4feb
Sportballislife <sport.ballislife@gmail.com>
2026-02-19 04:59:14 -0800

    .env update

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -1,4 +1,7 @@
 export const variables = {
-  API_URL: "http://localhost:45225/api/",
-  PHOTO_URL: "http://localhost:45225/Photos/",
+  // API_URL: "http://localhost:45225/api/",
+  // PHOTO_URL: "http://localhost:45225/Photos/",
+
+  API_URL: "https://forthubapi-production.up.railway.app/api/",
+  PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
 };
