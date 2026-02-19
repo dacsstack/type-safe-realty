8caf5170fd7dab677a7e82afe415c613cb40ef92
Sportballislife <sport.ballislife@gmail.com>
2026-02-19 10:33:33 -0800

    edit push

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ export default function Login({ setToken }) {
 
     try {
       const res = await fetch(
-        "https://forthubapi-production.up.railway.app/api/login",
+        "https://import.meta.env.VITE_API_URL/api/login",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
