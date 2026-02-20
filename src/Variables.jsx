6576c2093f51e9bfbee0f5fa809a12475a97c3fc
Sportballislife <sport.ballislife@gmail.com>
2026-02-20 07:35:48 -0800

    updates fetch

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -1,13 +1,17 @@
-// export const variables = {
-//   API_URL: "http://localhost:45225/api/",
-//   PHOTO_URL: "http://localhost:45225/Photos/",
+// // export const variables = {
+// //   API_URL: "http://localhost:45225/api/",
+// //   PHOTO_URL: "http://localhost:45225/Photos/",
 
-//   // API_URL: "https://forthubapi-production.up.railway.app/api/",
-//   // PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
-// };
-const BASE =
-  process.env.NODE_ENV === "production"
-    ? "https://forthub-backendapi-production.up.railway.app"
-    : "http://localhost:5000";
-export const API_URL = `${BASE}/api`;
-export const PHOTO_URL = `${BASE}/Photos`;
+// //   // API_URL: "https://forthubapi-production.up.railway.app/api/",
+// //   // PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
+// // };
+// const BASE =
+//   process.env.NODE_ENV === "production"
+//     ? "https://forthub-backendapi-production.up.railway.app"
+//     : "http://localhost:5000";
+// export const API_URL = `${BASE}/api`;
+// export const PHOTO_URL = `${BASE}/Photos`;
+export const variables = {
+  API_URL: import.meta.env.VITE_API_URL,
+  PHOTO_URL: import.meta.env.VITE_PHOTO_URL,
+};
