6f233fac290b1cf74760ed2303301d7a9506ff9b
Sportballislife <sport.ballislife@gmail.com>
2026-02-20 07:03:26 -0800

    updates fetch

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ export default function Login({ setToken }) {
 
     try {
       const res = await fetch(
-        "http://forthub-backendapi-production.up.railway.app/api/login",
+        "https://forthub-backendapi-production.up.railway.app/api/login",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
