14d920a18a582023d1cb7ee6af71cd05b7c81217
Sportballislife <sport.ballislife@gmail.com>
2026-02-21 07:53:07 -0800

    Change Var

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -1,16 +1,3 @@
-// export const variables = {
-//   API_URL: "https://forthub-backendapi-production.up.railway.app/api/",
-//   PHOTO_URL: "https://forthub-backendapi-production.up.railway.app/Photos/",
-// };
-// //   // API_URL: "https://forthubapi-production.up.railway.app/api/",
-// //   // PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
-// // };
-// // const BASE =
-// //   process.env.NODE_ENV === "production"
-// //     ? "https://forthub-backendapi-production.up.railway.app"
-// //     : "http://localhost:5000";
-// // export const API_URL = `${BASE}/api`;
-// // export const PHOTO_URL = `${BASE}/Photos`;
 export const variables = {
   API_URL: import.meta.env.VITE_API_URL,
   PHOTO_URL: import.meta.env.VITE_PHOTO_URL,
