28722823aa0cc372dfab31012b9312743c4c6e81
Sportballislife <sport.ballislife@gmail.com>
2026-02-20 23:33:07 -0800

    Updaten

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -1,18 +1,18 @@
-// // export const variables = {
-// //   API_URL: "https://forthub-backendapi-production.up.railway.app/api/",
-// //   PHOTO_URL: "https://forthub-backendapi-production.up.railway.app/Photos/",
-// // };
-// // //   // API_URL: "https://forthubapi-production.up.railway.app/api/",
-// // //   // PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
-// // // };
-// // // const BASE =
-// // //   process.env.NODE_ENV === "production"
-// // //     ? "https://forthub-backendapi-production.up.railway.app"
-// // //     : "http://localhost:5000";
-// // // export const API_URL = `${BASE}/api`;
-// // // export const PHOTO_URL = `${BASE}/Photos`;
 // export const variables = {
-//   VITE_API_URL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/",
-//   VITE_PHOTO_URL:
-//     import.meta.env.VITE_PHOTO_URL || "http://localhost:5000/Photos/",
+//   API_URL: "https://forthub-backendapi-production.up.railway.app/api/",
+//   PHOTO_URL: "https://forthub-backendapi-production.up.railway.app/Photos/",
 // };
+// //   // API_URL: "https://forthubapi-production.up.railway.app/api/",
+// //   // PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
+// // };
+// // const BASE =
+// //   process.env.NODE_ENV === "production"
+// //     ? "https://forthub-backendapi-production.up.railway.app"
+// //     : "http://localhost:5000";
+// // export const API_URL = `${BASE}/api`;
+// // export const PHOTO_URL = `${BASE}/Photos`;
+export const variables = {
+  VITE_API_URL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/",
+  VITE_PHOTO_URL:
+    import.meta.env.VITE_PHOTO_URL || "http://localhost:5000/Photos/",
+};
