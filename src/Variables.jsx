212ac9092fe0daa4a2d5d6d0048305aaf6580543
Sportballislife <sport.ballislife@gmail.com>
2026-02-21 07:33:18 -0800

    Change Var

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -12,6 +12,6 @@
 // // export const API_URL = `${BASE}/api`;
 // // export const PHOTO_URL = `${BASE}/Photos`;
 export const variables = {
-  API_URL: import.meta.env.VITE_API_URL,
-  PHOTO_URL: import.meta.env.VITE_PHOTO_URL,
+  API_URL: import.meta.env.VITE_API_URL || "/api/",
+  PHOTO_URL: import.meta.env.VITE_PHOTO_URL || "/Photo/",
 };
