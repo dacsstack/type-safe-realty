3880455f4239aa289b943e9ad4781e302d2f6ddd
Sportballislife <sport.ballislife@gmail.com>
2026-02-20 21:12:21 -0800

    add dist

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -1,4 +1,5 @@
 // export const variables = {
+<<<<<<< HEAD
 //   API_URL: "https://forthub-backendapi-production.up.railway.app/api/",
 //   PHOTO_URL: "https://forthub-backendapi-production.up.railway.app/Photos/",
 // };
@@ -15,3 +16,17 @@ export const variables = {
   API_URL: import.meta.env.VITE_API_URL,
   PHOTO_URL: import.meta.env.VITE_PHOTO_URL,
 };
+=======
+//   API_URL: "http://localhost:45225/api/",
+//   PHOTO_URL: "http://localhost:45225/Photos/",
+
+//   // API_URL: "https://forthubapi-production.up.railway.app/api/",
+//   // PHOTO_URL: "https://forthubapi-production.up.railway.app/Photos/",
+// };
+const BASE =
+  process.env.NODE_ENV === "production"
+    ? "https://forthub-backendapi-production.up.railway.app"
+    : "http://localhost:5000";
+export const API_URL = `${BASE}/api`;
+export const PHOTO_URL = `${BASE}/Photos`;
+>>>>>>> badca22 (fix)
